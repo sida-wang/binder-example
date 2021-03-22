5d8e1700cb7fbce31068e059be750549c0f0af04
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2021-03-22 16:29:55 +1100

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -84,8 +84,8 @@ for var in ['var1', 'var2', 'var3']:
     
 
 def update_plot(attrname, old, new):
-    p.title.text=f'{new[0]},{new[1]}'
-    #gdf.active = (gdf['0'] >= new[0]) & (gdf['0'] <= new[1])
+    gdf.active = (gdf['0'] >= new[0]) & (gdf['0'] <= new[1])
+    p.title.text=f'{np.sum(gdf.active)}'
     #geosource.geojson = gdf.to_json()
 
 filter_list['var1'].slider_.on_change('value',update_plot)
