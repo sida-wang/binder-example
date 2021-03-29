02f50b5ad2d50f58d0734a4e83a511c3e1e88774
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2021-03-29 17:10:37 +1100

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -49,10 +49,9 @@ ypoints = np.random.randint(Y_RANGE[0],Y_RANGE[1],npoints)
 test_points = [Point(i) for i in zip(xpoints, ypoints)]
 
 gdf = gpd.GeoDataFrame({'var1':np.random.randint(0,100,npoints)}, geometry=test_points)
-gdf['active'] = True
 geosource = GeoJSONDataSource(geojson=gdf.to_json())
 
-test_view = CDSView(source=geosource, filters=[BooleanFilter(booleans=gdf.active)])
+test_view = CDSView(source=geosource, filters=[BooleanFilter(booleans=np.full(len(gdf),True,dtype=bool)])
 
 tile_provider = get_provider('CARTODBPOSITRON')
 
@@ -82,8 +81,10 @@ for var in ['var1', 'var2', 'var3']:
     
 
 def update_plot(attrname, old, new):
-    gdf.active = (gdf.var1 >= new[0]) & (gdf.var1 <= new[1])
-    test_view.filters[0] = BooleanFilter(booleans=gdf.active)
+    mask = np.full(len(gdf),True,dtype=bool)
+    mask = mask & (gdf.var1 >= new[0]) & (gdf.var1 <= new[1])
+    #gdf.active = (gdf.var1 >= new[0]) & (gdf.var1 <= new[1])
+    test_view.filters[0] = BooleanFilter(booleans=mask)
 
 filter_list['var1'].slider_.on_change('value',update_plot)
     
